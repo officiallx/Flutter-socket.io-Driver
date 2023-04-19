78866f0ab12ed41e08df12994b683e25a28dd167
Elex Maharjan <chimprockofficial@gmail.com>
2023-04-19 11:39:19 +0545

    minor bugs fixed | version update

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,12 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:location/location.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class HomePage extends StatefulWidget {
@@ -17,6 +24,8 @@ class _HomePageState extends State<HomePage> {
   late LocationData _locationData;
 
   Future getLocation() async {
+    location.enableBackgroundMode(enable: true);
+
     _serviceEnabled = await location.serviceEnabled();
     if (!_serviceEnabled) {
       _serviceEnabled = await location.requestService();
@@ -31,8 +40,10 @@ class _HomePageState extends State<HomePage> {
       if (_permissionGranted != PermissionStatus.granted) {
         return;
       }
+    } else if (_permissionGranted == PermissionStatus.deniedForever) {
+      _permissionGranted =
+          _permissionGranted = await location.requestPermission();
     }
-
     _locationData = await location.getLocation();
 
     socket.emit('location', {
@@ -45,12 +56,7 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     connectSocketIO();
     getLocation();
-    location.onLocationChanged.listen((LocationData currentLocation) {
-      socket.emit('location', {
-        'longitude': currentLocation.longitude,
-        'latitude': currentLocation.latitude
-      });
-    });
+    listenToCurrentLocation();
     super.initState();
   }
 
@@ -60,15 +66,147 @@ class _HomePageState extends State<HomePage> {
     super.dispose();
   }
 
+  listenToCurrentLocation() {
+    location.onLocationChanged.listen((LocationData currentLocation) {
+      socket.emit('location', {
+        'longitude': currentLocation.longitude,
+        'latitude': currentLocation.latitude
+      });
+    });
+  }
+
+  Future<void> initializeService() async {
+    final service = FlutterBackgroundService();
+
+    /// OPTIONAL, using custom notification channel id
+    // const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    //   'my_foreground', // id
+    //   'MY FOREGROUND SERVICE', // title
+    //   description:
+    //       'This channel is used for important notifications.', // description
+    //   importance: Importance.low, // importance must be at low or higher level
+    // );
+
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+
+    if (Platform.isIOS) {
+      await flutterLocalNotificationsPlugin.initialize(
+        const InitializationSettings(
+          iOS: DarwinInitializationSettings(),
+        ),
+      );
+    }
+
+    // await flutterLocalNotificationsPlugin
+    //     .resolvePlatformSpecificImplementation<
+    //         AndroidFlutterLocalNotificationsPlugin>()
+    //     ?.createNotificationChannel(channel);
+
+    await service.configure(
+      androidConfiguration: AndroidConfiguration(
+        onStart: onStart,
+        autoStart: false,
+        isForegroundMode: true,
+        notificationChannelId: 'my_foreground',
+        initialNotificationTitle: 'MPA',
+        initialNotificationContent: 'Location on use',
+        foregroundServiceNotificationId: 888,
+      ),
+      iosConfiguration: IosConfiguration(
+        autoStart: false,
+        onForeground: onStart,
+        onBackground: onIosBackground,
+      ),
+    );
+
+    service.startService();
+  }
+
+// to ensure this is executed
+// run app from xcode, then from xcode menu, select Simulate Background Fetch
+
+  @pragma('vm:entry-point')
+  Future<bool> onIosBackground(ServiceInstance service) async {
+    WidgetsFlutterBinding.ensureInitialized();
+    DartPluginRegistrant.ensureInitialized();
+
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    await preferences.reload();
+    final log = preferences.getStringList('log') ?? <String>[];
+    log.add(DateTime.now().toIso8601String());
+    await preferences.setStringList('log', log);
+
+    return true;
+  }
+
+  @pragma('vm:entry-point')
+  void onStart(ServiceInstance service) async {
+    DartPluginRegistrant.ensureInitialized();
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    await preferences.setString("hello", "world");
+
+    /// OPTIONAL when use custom notification
+    // final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    //     FlutterLocalNotificationsPlugin();
+
+    if (service is AndroidServiceInstance) {
+      service.on('setAsForeground').listen((event) {
+        service.setAsForegroundService();
+      });
+
+      service.on('setAsBackground').listen((event) {
+        service.setAsBackgroundService();
+      });
+    }
+
+    service.on('stopService').listen((event) {
+      service.stopSelf();
+    });
+  }
+
+  String text = "Start Service";
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: const [
-              Text("Hello"),
+            children: [
+              ElevatedButton(
+                child: const Text("Foreground Mode"),
+                onPressed: () {
+                  FlutterBackgroundService().invoke("setAsForeground");
+                },
+              ),
+              ElevatedButton(
+                child: const Text("Background Mode"),
+                onPressed: () {
+                  FlutterBackgroundService().invoke("setAsBackground");
+                },
+              ),
+              ElevatedButton(
+                child: Text(text),
+                onPressed: () async {
+                  final service = FlutterBackgroundService();
+                  var isRunning = await service.isRunning();
+                  if (isRunning) {
+                    service.invoke("stopService");
+                  } else {
+                    service.startService();
+                  }
+                  if (!isRunning) {
+                    text = 'Stop Service';
+                  } else {
+                    text = 'Start Service';
+                  }
+                  setState(() {});
+                },
+              ),
+              const Expanded(
+                child: LogView(),
+              ),
             ],
           ),
         ),
@@ -78,21 +216,61 @@ class _HomePageState extends State<HomePage> {
 
   void connectSocketIO() {
     try {
-      socket = IO.io('http://172.25.6.21:5000',
+      // socket = IO.io('https://location-service-mpa.herokuapp.com',
+      //     IO.OptionBuilder().setTransports(['websocket']).build());
+      socket = IO.io('http://192.168.18.127:5050',
           IO.OptionBuilder().setTransports(['websocket']).build());
       socket.onConnect((_) {
         print('SOCKET connected');
-        socket.emit('chat message', 'SOCKET connected');
       });
       socket.onDisconnect((_) {
         print('SOCKET: ${socket.id}');
-        socket.emit('chat message', 'SOCKET disconnected');
       });
       socket.onDisconnect((_) => print('disconnect'));
-      //socket.on('chat message', (data) => print(data));
-      //socket.on('location', (data) => print(data));
     } catch (ex) {
       print(ex);
     }
+  }
+}
+
+class LogView extends StatefulWidget {
+  const LogView({Key? key}) : super(key: key);
+
+  @override
+  State<LogView> createState() => _LogViewState();
+}
+
+class _LogViewState extends State<LogView> {
+  late final Timer timer;
+  List<String> logs = [];
+
+  @override
+  void initState() {
+    super.initState();
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) async {
+      final SharedPreferences sp = await SharedPreferences.getInstance();
+      await sp.reload();
+      logs = sp.getStringList('log') ?? [];
+      if (mounted) {
+        setState(() {});
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: logs.length,
+      itemBuilder: (context, index) {
+        final log = logs.elementAt(index);
+        return Text(log);
+      },
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'homepage.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
