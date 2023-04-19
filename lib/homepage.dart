1b00fcde9923cee9f751be5f87fcfeac6b412bcc
Elex Maharjan <chimprockofficial@gmail.com>
2023-04-19 13:17:34 +0545

    minor bugs fixed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,12 +1,6 @@
 import 'dart:async';
-import 'dart:io';
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:flutter_background_service_android/flutter_background_service_android.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:location/location.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class HomePage extends StatefulWidget {
@@ -23,17 +17,7 @@ class _HomePageState extends State<HomePage> {
   late PermissionStatus _permissionGranted;
   late LocationData _locationData;
 
-  Future getLocation() async {
-    location.enableBackgroundMode(enable: true);
-
-    _serviceEnabled = await location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await location.requestService();
-      if (!_serviceEnabled) {
-        return;
-      }
-    }
-
+  Future<void> getLocationPermission() async {
     _permissionGranted = await location.hasPermission();
     if (_permissionGranted == PermissionStatus.denied) {
       _permissionGranted = await location.requestPermission();
@@ -41,22 +25,33 @@ class _HomePageState extends State<HomePage> {
         return;
       }
     } else if (_permissionGranted == PermissionStatus.deniedForever) {
-      _permissionGranted =
-          _permissionGranted = await location.requestPermission();
+      _permissionGranted = await location.requestPermission();
     }
-    _locationData = await location.getLocation();
+  }
 
-    socket.emit('location', {
-      'longitude': _locationData.longitude,
-      'latitude': _locationData.latitude
-    });
+  Future<void> getLocation() async {
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+    if (_serviceEnabled) {
+      await getLocationPermission();
+      _locationData = await location.getLocation();
+
+      socket.emit('location', {
+        'longitude': _locationData.longitude,
+        'latitude': _locationData.latitude
+      });
+    }
   }
 
   @override
   void initState() {
     connectSocketIO();
-    getLocation();
-    listenToCurrentLocation();
+    getLocation().whenComplete(() => listenToCurrentLocation());
     super.initState();
   }
 
@@ -75,139 +70,13 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  Future<void> initializeService() async {
-    final service = FlutterBackgroundService();
-
-    /// OPTIONAL, using custom notification channel id
-    // const AndroidNotificationChannel channel = AndroidNotificationChannel(
-    //   'my_foreground', // id
-    //   'MY FOREGROUND SERVICE', // title
-    //   description:
-    //       'This channel is used for important notifications.', // description
-    //   importance: Importance.low, // importance must be at low or higher level
-    // );
-
-    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-        FlutterLocalNotificationsPlugin();
-
-    if (Platform.isIOS) {
-      await flutterLocalNotificationsPlugin.initialize(
-        const InitializationSettings(
-          iOS: DarwinInitializationSettings(),
-        ),
-      );
-    }
-
-    // await flutterLocalNotificationsPlugin
-    //     .resolvePlatformSpecificImplementation<
-    //         AndroidFlutterLocalNotificationsPlugin>()
-    //     ?.createNotificationChannel(channel);
-
-    await service.configure(
-      androidConfiguration: AndroidConfiguration(
-        onStart: onStart,
-        autoStart: false,
-        isForegroundMode: true,
-        notificationChannelId: 'my_foreground',
-        initialNotificationTitle: 'MPA',
-        initialNotificationContent: 'Location on use',
-        foregroundServiceNotificationId: 888,
-      ),
-      iosConfiguration: IosConfiguration(
-        autoStart: false,
-        onForeground: onStart,
-        onBackground: onIosBackground,
-      ),
-    );
-
-    service.startService();
-  }
-
-// to ensure this is executed
-// run app from xcode, then from xcode menu, select Simulate Background Fetch
-
-  @pragma('vm:entry-point')
-  Future<bool> onIosBackground(ServiceInstance service) async {
-    WidgetsFlutterBinding.ensureInitialized();
-    DartPluginRegistrant.ensureInitialized();
-
-    SharedPreferences preferences = await SharedPreferences.getInstance();
-    await preferences.reload();
-    final log = preferences.getStringList('log') ?? <String>[];
-    log.add(DateTime.now().toIso8601String());
-    await preferences.setStringList('log', log);
-
-    return true;
-  }
-
-  @pragma('vm:entry-point')
-  void onStart(ServiceInstance service) async {
-    DartPluginRegistrant.ensureInitialized();
-    SharedPreferences preferences = await SharedPreferences.getInstance();
-    await preferences.setString("hello", "world");
-
-    /// OPTIONAL when use custom notification
-    // final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    //     FlutterLocalNotificationsPlugin();
-
-    if (service is AndroidServiceInstance) {
-      service.on('setAsForeground').listen((event) {
-        service.setAsForegroundService();
-      });
-
-      service.on('setAsBackground').listen((event) {
-        service.setAsBackgroundService();
-      });
-    }
-
-    service.on('stopService').listen((event) {
-      service.stopSelf();
-    });
-  }
-
-  String text = "Start Service";
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Column(
-            children: [
-              ElevatedButton(
-                child: const Text("Foreground Mode"),
-                onPressed: () {
-                  FlutterBackgroundService().invoke("setAsForeground");
-                },
-              ),
-              ElevatedButton(
-                child: const Text("Background Mode"),
-                onPressed: () {
-                  FlutterBackgroundService().invoke("setAsBackground");
-                },
-              ),
-              ElevatedButton(
-                child: Text(text),
-                onPressed: () async {
-                  final service = FlutterBackgroundService();
-                  var isRunning = await service.isRunning();
-                  if (isRunning) {
-                    service.invoke("stopService");
-                  } else {
-                    service.startService();
-                  }
-                  if (!isRunning) {
-                    text = 'Stop Service';
-                  } else {
-                    text = 'Start Service';
-                  }
-                  setState(() {});
-                },
-              ),
-              const Expanded(
-                child: LogView(),
-              ),
-            ],
+          child: Center(
+            child: Text("MPA"),
           ),
         ),
       ),
@@ -230,47 +99,5 @@ class _HomePageState extends State<HomePage> {
     } catch (ex) {
       print(ex);
     }
-  }
-}
-
-class LogView extends StatefulWidget {
-  const LogView({Key? key}) : super(key: key);
-
-  @override
-  State<LogView> createState() => _LogViewState();
-}
-
-class _LogViewState extends State<LogView> {
-  late final Timer timer;
-  List<String> logs = [];
-
-  @override
-  void initState() {
-    super.initState();
-    timer = Timer.periodic(const Duration(seconds: 1), (timer) async {
-      final SharedPreferences sp = await SharedPreferences.getInstance();
-      await sp.reload();
-      logs = sp.getStringList('log') ?? [];
-      if (mounted) {
-        setState(() {});
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    timer.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: logs.length,
-      itemBuilder: (context, index) {
-        final log = logs.elementAt(index);
-        return Text(log);
-      },
-    );
   }
 }
